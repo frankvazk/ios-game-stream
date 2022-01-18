6652a04058139197b9bd1175f11b483bb067a9dc
Francisco Vazquez <frankvazk@gmail.com>
2022-01-17 18:30:34 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Francisco Vazquez on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

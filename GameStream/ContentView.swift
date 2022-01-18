6a86898d82e800abf378e367644b495ec417d174
Francisco Vazquez <frankvazk@gmail.com>
2022-01-17 19:16:35 -0600

    01 Background and Logo

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 19/255, green: 30/255, blue: 53/255, opacity: 1.0)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("applogo")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit
+                    )
+                .frame(width: 250)
+                InicioYRegistroView()
+            }
+            
+        }
+    }
+}
+
+struct InicioYRegistroView: View {
+    var body: some View{
+        VStack{
+            HStack{
+                Text("INICIA SESIÓN")
+                Text("REGÍSTRATE")
+                    .foregroundColor(Color.white)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla01")
+            .resizable()
+            .aspectRatio(
+                contentMode: .fill
+            )
         ContentView()
     }
 }

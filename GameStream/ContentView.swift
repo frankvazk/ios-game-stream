e552a4ec8226eda6e59b63f2f579aebbd7bca4a1
Francisco Vazquez <frankvazk@gmail.com>
2022-02-03 19:12:42 -0600

    Modules added: Suggested Categories, Recommended Games and Suggested Games

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 }
                 
             }
-            .navigationBarHidden(true)
+            .navigationBarHidden(true)            
         }
     }
 }

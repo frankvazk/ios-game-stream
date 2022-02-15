a2ac5e0359a1a2705b53b87d60a7f2e0526c5a93
Francisco Vazquez <frankvazk@gmail.com>
2022-02-14 18:43:06 -0600

    GamelModel Definition

--- GameStream/Model/GameModel.swift
+++ GameStream/Model/GameModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Games : Codable {
+    var games:[Game]
+}
+
+struct Game : Codable {
+    var title:String
+    var studio:String
+    var contentRaiting:String
+    var publicationYear:String
+    var description:String
+    var platforms:[String]
+    var tags:[String]
+    var videosUrls:VideoUrl
+    var galleryImages:[String]
+}
+
+struct VideoUrl : Codable {
+    var mobile:String
+    var tables:String
+}

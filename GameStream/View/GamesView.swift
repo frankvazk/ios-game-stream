ec32188b0855b525ac13583ee678a9fb5d6f8b31
Francisco Vazquez <frankvazk@gmail.com>
2022-02-17 20:11:43 -0600

    Showing games title  into the GamesView using LazyVGrid

--- GameStream/View/GamesView.swift
+++ GameStream/View/GamesView.swift
@@ -9,10 +9,55 @@ import SwiftUI
 
 struct GamesView: View {
     @ObservedObject var videoGames = GamesViewModel()
-
+    @State var gameViewIsActive : Bool = false //Esta bandera servirá para mostrar la vista de Game
+    @State var url : String = ""
+    @State var title : String = ""
+    @State var studio : String = ""
+    @State var contentRating : String = ""
+    @State var publishedYear : String = ""
+    @State var description : String = ""
+    @State var tags:[String] = []
+    @State var imgsUrl:[String] = []
+    
+    //Con este comeponente podemos definir una matriz para la presentación de los datos
+    //Para este ejemplo la matriz sera de dos columnas por N filas dependiendo el numero de
+    //items que se recuperen de la petición al servidorq
+    let gridShape = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    
     var body: some View {
-        VStack{
-            Text("Hola desde la pantalla HomeView!")
+        ZStack{
+            Color("Marine").ignoresSafeArea()
+            VStack{
+                Text("Juegos")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 64, trailing: 0))
+                
+                ScrollView(showsIndicators: false){
+                    LazyVGrid(columns: gridShape, spacing: 8) {
+                        ForEach(self.videoGames.gamesInfo, id:\.self){
+                            game in
+                            Button {
+                                self.url = game.videosUrls.mobile
+                                self.title = game.title
+                                self.studio = game.studio
+                                self.contentRating = game.contentRaiting
+                                self.description = game.description
+                                self.tags = game.tags
+                                self.imgsUrl = game.galleryImages
+                                print("Pulse el juego \(self.title)")
+                            } label: {
+                                Text(game.title)
+                            }
+
+                        }
+                    }
+                }
+            }.padding(.horizontal, 18.0)
         }
         .navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
